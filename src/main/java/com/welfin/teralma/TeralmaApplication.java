8a7de88a50a9f2805fac819e54890717d035c1c9
Moussa Ba <moussa10ba@gmail.com>
2022-01-20 20:37:02 +0000

    First Commit

---
+++ src/main/java/com/welfin/teralma/TeralmaApplication.java
@@ -0,0 +1,13 @@
+package com.welfin.teralma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeralmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeralmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welfin/teralma/TeralmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welfin.teralma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeralmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
